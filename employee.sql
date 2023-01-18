b5a05a99cd4af8478b695588b14e2d541745e3bc
Holden Matthews <holdenmatthews1@gmail.com>
2023-01-17 17:16:12 -0700

    third commit

--- employee.sql
+++ employee.sql
@@ -6,9 +6,11 @@ SELECT MAX(birth_date) FROM employee;
 
 SELECT MIN(birth_date) FROM employee;
 
-SELECT * 
-FROM employee
-WHERE reports_to = 2;
+SELECT * FROM employee
+WHERE reports_to = (
+    SELECT employee_id FROM employee
+    WHERE first_name = 'Nancy'
+);
 
 SELECT COUNT(*)
 FROM employee
